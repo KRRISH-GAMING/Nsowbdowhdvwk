32e432786cdc10a10d123aa5ac107453e47ae5fd
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-12-22 16:36:05 +0530

    Add files via upload

--- plugins/config.py
+++ plugins/config.py
@@ -1,13 +1,13 @@
 from os import path, getenv
 
-API_ID = int(getenv("API_ID", "15479023"))
-API_HASH = getenv("API_HASH", "f8f6cf547822449c29fc60dae3b31dd4")
+API_ID = int(getenv("API_ID", "35384565"))
+API_HASH = getenv("API_HASH", "dbba8a136120df358bd3b6e1fbc18792")
 BOT_TOKEN = getenv("BOT_TOKEN", "8060569801:AAEk1tmmpaUfIyydSe20TASfscafotRbqKA")
 
 # Your Force Subscribe Channel Id Below 
 CHANNEL = int(getenv("CHANNEL", "-1002829948273")) # Make Bot Admin In This Channel
 
 # Admin Or Owner Id Below
-ADMIN = list(map(int, getenv("ADMIN", "1512442581").split()))
+ADMIN = list(map(int, getenv("ADMIN", "8477930865").split()))
 
 MONGO_URI = getenv("MONGO_URI", "mongodb+srv://KM-AutoAccept:KM-AutoAccept123@km-autoaccept.restswy.mongodb.net/?retryWrites=true&w=majority&appName=KM-AutoAccept")
